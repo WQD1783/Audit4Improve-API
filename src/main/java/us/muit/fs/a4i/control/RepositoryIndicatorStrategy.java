ea3930be4345d66e8ad0c6f883a432014d40ca55
Sindre Øyen <161310938+CHR8486@users.noreply.github.com>
2024-05-30 20:32:29 +0200

    test: create tests for RepositoryIndicatorStrategy

--- src/main/java/us/muit/fs/a4i/control/RepositoryIndicatorStrategy.java
+++ src/main/java/us/muit/fs/a4i/control/RepositoryIndicatorStrategy.java
@@ -12,7 +12,51 @@ public class RepositoryIndicatorStrategy implements IndicatorStrategy<Double> {
     // MARK: - Attributes
     private static Logger log = Logger.getLogger(Indicator.class.getName());
     // The metrics needed to calculate the indicator output
-    private static final List<String> REQUIRED_METRICS = Arrays.asList("queryCommits", "issues", "branches", "pullRequests");
+    private static final List<String> REQUIRED_METRICS = Arrays.asList(
+        "conventionalCommits", 
+        "commitsWithDescription", 
+        "issuesWithLabels", 
+        "gitFlowBranches", 
+        "conventionalPullRequests");
+    private static final String id = "RepositoryIndicatorStrategy";
+
+    // Weights for each metric
+    private static double WEIGHT_CONVENTIONAL_COMMITS = 0.2;
+    private static double WEIGHT_COMMITS_WITH_DESCRIPTION = 0.2;
+    private static double WEIGHT_ISSUES_WITH_LABELS = 0.2;
+    private static double WEIGHT_GIT_FLOW_BRANCHES = 0.2;
+    private static double WEIGHT_CONVENTIONAL_PULL_REQUESTS = 0.2;
+
+    // MARK: - Constructor
+    /**
+     * Constructor. Default weights are equal for all metrics (0.2).
+     */
+    public RepositoryIndicatorStrategy() {
+        super();
+    }
+    /**
+     * Constructor. Set the weights for each metric. The sum of the weights must be equal to 1.
+     * @param weightConventionalCommits the weight for the conventional commits metric
+     * @param weightCommitsWithDescription the weight for the commits with description metric
+     * @param weightIssuesWithLabels the weight for the issues with labels metric
+     * @param weightGitFlowBranches the weight for the git flow branches metric
+     * @param weightConventionalPullRequests the weight for the conventional pull requests metric
+     */
+    public RepositoryIndicatorStrategy(Double weightConventionalCommits, Double weightCommitsWithDescription, 
+        Double weightIssuesWithLabels, Double weightGitFlowBranches, 
+        Double weightConventionalPullRequests) throws IllegalArgumentException {
+        // Control that the sum of the weights is equal to 1
+        if (weightConventionalCommits + weightCommitsWithDescription + weightIssuesWithLabels + weightGitFlowBranches + weightConventionalPullRequests != 1) {
+            throw new IllegalArgumentException("The sum of the weights must be equal to 1");
+        }
+        
+        // Set the weights
+        this.WEIGHT_CONVENTIONAL_COMMITS = weightConventionalCommits;
+        this.WEIGHT_COMMITS_WITH_DESCRIPTION = weightCommitsWithDescription;
+        this.WEIGHT_ISSUES_WITH_LABELS = weightIssuesWithLabels;
+        this.WEIGHT_GIT_FLOW_BRANCHES = weightGitFlowBranches;
+        this.WEIGHT_CONVENTIONAL_PULL_REQUESTS = weightConventionalPullRequests;
+    }
 
     // MARK: - Implemented methods
     // Calculate the indicator output based on the provided metrics
